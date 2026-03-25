87d3e8f7fd01c1bc52edfde8b9b968f75d158f30
Camilo685 <46032121+Camilo685@users.noreply.github.com>
2026-03-25 17:10:27 -0500

    Create UI for the rock, paper, scissors web game

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
- let getRandomInt = (max) => Math.floor(Math.random() * max);
- let getComputerChoice = (randomN) => (randomN == 0) ? "rock" : (randomN == 1) ? "paper" : "scissor";
- let getHumanChoice = () => prompt("Rock, paper or scissors?", "null");
+let options = document.querySelectorAll("button");
+let winP = document.querySelector(".win-header").querySelector("p");
+let tieP = document.querySelector(".tie-header").querySelector("p");
+let loseP = document.querySelector(".lose-header").querySelector("p");
+let resultsP = document.querySelector(".results-container").querySelector("p");
 
- let keepPlaying = true;
- let score = [0, 0, 0];
- let scoreMessage = (scre) => `Current score: Wins = ${scre[0]}, Ties = ${scre[1]}, Loses = ${scre[2]}`;
- while (keepPlaying){
-    let choice = getHumanChoice();
-    if (choice) {
-        if (choice != "rock" && choice != "paper" && choice != "scissor") {
-            console.log("Invalid option");
+let getRandomInt = (max) => Math.floor(Math.random() * max);
+
+options.forEach((button, humanChoice) => button.addEventListener('click', () => {
+    let computerChoice = getRandomInt(3);
+
+    if (computerChoice == humanChoice){
+        resultsP.textContent = "It's a tie!";
+        tieP.textContent = +tieP.textContent + 1;
+    } else if (computerChoice == 0) {
+        if (humanChoice == 1) {
+            resultsP.textContent = "Paper beats rock, you win!";
+            winP.textContent = +winP.textContent + 1;
         } else {
-            let computerChoice = getComputerChoice(getRandomInt(3));
-            if (computerChoice == choice){
-                console.log("Tie!");
-                score[1] += 1;
-            } else if (computerChoice == "rock") {
-                if (choice == "paper") {
-                    console.log("Win!");
-                    score[0] += 1;
-                } else {
-                    console.log("Lose!");
-                    score[2] += 1;
-                }
-            } else if (computerChoice == "paper") {
-                if (choice == "rock") {
-                    console.log("Lose!");
-                    score[2] += 1;
-                } else {
-                    console.log("Win!");
-                    score[0] += 1;
-                }
-            } else {
-                if (choice == "paper") {
-                    console.log("Lose!");
-                    score[2] += 1;
-                } else {
-                    console.log("Win!");
-                    score[0] += 1;
-                }
-            }
-            console.log(scoreMessage(score));
+            resultsP.textContent = "Rock beats scissors, you lose!";
+            loseP.textContent = +loseP.textContent + 1;
+        }
+    } else if (computerChoice == 1) {
+        if (humanChoice == 0) {
+            resultsP.textContent = "Paper beats rock, you lose!";
+            loseP.textContent = +loseP.textContent + 1;
+        } else {
+            resultsP.textContent = "Scissors beats paper, you win!";
+            winP.textContent = +winP.textContent + 1;
         }
     } else {
-        keepPlaying = false;
-        console.log("Thanks for playing! " + scoreMessage(score).replace("Current", "Final"));
+        if (humanChoice == 1) {
+            resultsP.textContent = "Scissors beats paper, you lose!";
+            loseP.textContent = +loseP.textContent + 1;
+        } else {
+            resultsP.textContent = "Rock beats scissors, you win!";
+            winP.textContent = +winP.textContent + 1;
+        }
     }
- }
+}));
